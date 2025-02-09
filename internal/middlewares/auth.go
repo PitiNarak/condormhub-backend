b019ceed983ab0a293b3132deb4bec4039db97be
Thanapon Johdee <sern.dev@gmail.com>
2025-02-10 06:59:57 +0700

    feat: auth middleware

---
+++ internal/middlewares/auth.go
@@ -0,0 +1,61 @@
+package middlewares
+
+import (
+	"errors"
+	"time"
+
+	"github.com/PitiNarak/condormhub-backend/internal/repositories"
+	"github.com/PitiNarak/condormhub-backend/pkg/error_handler"
+	"github.com/PitiNarak/condormhub-backend/pkg/utils"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
+
+type AuthMiddleware struct {
+	jwtUtils *utils.JWTUtils
+	userRepo *repositories.UserRepo
+}
+
+func NewAuthMiddleware(jwtUtils *utils.JWTUtils, userRepo *repositories.UserRepo) *AuthMiddleware {
+	return &AuthMiddleware{
+		jwtUtils: jwtUtils,
+		userRepo: userRepo,
+	}
+}
+
+func (a *AuthMiddleware) Auth(ctx *fiber.Ctx) error {
+	authHeader := ctx.Get("Authorization")
+
+	if authHeader == "" {
+		return error_handler.UnauthorizedError(errors.New("request without authorization header"), "Authorization header is required")
+	}
+
+	if len(authHeader) < 7 || authHeader[:6] != "Bearer" {
+		return error_handler.UnauthorizedError(errors.New("invalid authorization header"), "Authorization header is invalid")
+	}
+
+	token := authHeader[7:]
+	claims, err := a.jwtUtils.DecodeJWT(token)
+	if err != nil {
+		return error_handler.UnauthorizedError(err, "Invalid token")
+	}
+
+	if claims.GetExp() < time.Now().Unix() {
+		return error_handler.UnauthorizedError(errors.New("token expired"), "Token is expired")
+	}
+
+	uuid, err := uuid.Parse(claims.GetUserID())
+	if err != nil {
+		return error_handler.UnauthorizedError(err, "Invalid user ID")
+	}
+
+	user, err := a.userRepo.GetUser(uuid)
+	if err != nil {
+		return error_handler.UnauthorizedError(err, "User not found")
+	}
+
+	ctx.Locals("userID", claims.GetUserID())
+	ctx.Locals("user", user)
+
+	return ctx.Next()
+}
