913e621290aa363a85aec62acb1aa8881b60aece
Thanapon Johdee <sern.dev@gmail.com>
2025-02-10 06:01:44 +0700

    refactor: create jwtUtils

--- pkg/utils/jwt.go
+++ pkg/utils/jwt.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -12,6 +14,71 @@ type JWTConfig struct {
 	Expiration   int    `env:"EXPIRATION_HOURS,required"`
 }
 
+type JWTUtils struct {
+	Config *JWTConfig
+}
+
+type JWTClaims struct {
+	jwt.RegisteredClaims
+	UserID string `json:"userID"`
+}
+
+type JWTClaimsInterface interface {
+	GetUserID() string
+	GetExp() int64
+	GetIat() int64
+}
+
+func NewJWTUtils(config *JWTConfig) *JWTUtils {
+	return &JWTUtils{Config: config}
+}
+
+func (j *JWTClaims) GetUserID() string {
+	return j.UserID
+}
+
+func (j *JWTClaims) GetExp() int64 {
+	return j.ExpiresAt.Unix()
+}
+
+func (j *JWTClaims) GetIat() int64 {
+	return j.IssuedAt.Unix()
+}
+
+func (j *JWTUtils) GenerateJWT(userID uuid.UUID) (string, error) {
+	claims := &JWTClaims{
+		UserID: userID.String(),
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * time.Duration(j.Config.Expiration))),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return jwtToken.SignedString([]byte(j.Config.JWTSecretKey))
+}
+
+func (j *JWTUtils) DecodeJWT(inputToken string) (*JWTClaims, error) {
+	token, err := jwt.ParseWithClaims(inputToken, &JWTClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(j.Config.JWTSecretKey), nil
+	})
+
+	if err != nil || !token.Valid {
+		return new(JWTClaims), errors.New("parse token failed")
+	}
+
+	claims, ok := token.Claims.(*JWTClaims)
+	if !ok || !token.Valid {
+		return new(JWTClaims), errors.New("invalid token")
+	}
+
+	return claims, nil
+}
+
+// Deprecated: Use JWTUtils.DecodeJWT instead.
 func GenerateJWT(userID uuid.UUID, config *JWTConfig) (string, error) {
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
@@ -21,6 +88,7 @@ func GenerateJWT(userID uuid.UUID, config *JWTConfig) (string, error) {
 	return token.SignedString([]byte(config.JWTSecretKey))
 }
 
+// Deprecated: Use JWTUtils.DecodeJWT instead.
 func DecodeJWT(inputToken string, config *JWTConfig) (*jwt.MapClaims, error) {
 	token, err := jwt.Parse(inputToken, func(token *jwt.Token) (interface{}, error) {
 		return []byte(config.JWTSecretKey), nil
