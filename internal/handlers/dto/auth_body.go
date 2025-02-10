b8b8cba456c1f4d982fa72a1514922e79a1c54f3
Chantouch Narasetaphan <inecrft4747@gmail.com>
2025-02-10 20:38:18 +0700

    refactor: rename dto files

--- internal/handlers/dto/auth_body.go
+++ internal/handlers/dto/auth_body.go
@@ -1,13 +1,5 @@
 package dto
 
-type ResetPasswordBody struct {
-	Email string `json:"email" validate:"required,email"`
-}
-
-type ResponseResetPasswordBody struct {
-	Password string `json:"password" validate:"required"`
-}
-
 type RegisterRequestBody struct {
 	Password string `json:"password" validate:"required"`
 	Email    string `json:"email" validate:"required,email"`
