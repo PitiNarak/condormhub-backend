cacd8808e95de9143691a36ac44b5e5da3fb8daa
bRight36691 <141995847+bRight36691@users.noreply.github.com>
2025-02-11 16:58:16 +0700

    remove unuse struct

--- internal/core/domain/user.go
+++ internal/core/domain/user.go
@@ -35,37 +35,8 @@ type User struct {
 	IsStudentVerified bool   `gorm:"default:false" json:"isStudentVerified"`
 }
 
+// Deprecated: move to dto instead
 type LoginRequest struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
-}
-
-type GetInfoRequest struct {
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required"`
-}
-
-type UserInfo struct {
-	UserName           string    `json:"username" gorm:"unique" validate:"required"`
-	Email              string    `json:"email" gorm:"unique" validate:"required,email"`
-	Firstname          string    `json:"firstname"`
-	Lastname           string    `json:"lastname"`
-	NationalID         string    `json:"nationalID" `
-	Gender             string    `json:"gender"`
-	BirthDate          time.Time `json:"birthDate"`
-	IsVerified         bool      `gorm:"default:false" json:"isVerified"`
-	Role               Role      `json:"role"`
-	StudentEvidence    string    `json:"studentEvidence"`
-	IsStudentVerified  bool      `gorm:"default:false" json:"isStudentVerified"`
-	FilledPersonalInfo bool      `gorm:"default:false" json:"filledPersonalInfo"`
-}
-
-type UpdateInfo struct {
-	Firstname       string    `json:"firstname"`
-	Lastname        string    `json:"lastname"`
-	NationalID      string    `json:"nationalID" `
-	Gender          string    `json:"gender"`
-	BirthDate       time.Time `json:"birthDate"`
-	Role            Role      `json:"role"`
-	StudentEvidence string    `json:"studentEvidence"`
 }
