e3b4f783bfd76e77bbfc32481d3b407be373073f
bRight36691 <141995847+bRight36691@users.noreply.github.com>
2025-02-13 00:26:59 +0700

    remove unused body

--- internal/handlers/dto/user_body.go
+++ internal/handlers/dto/user_body.go
@@ -30,17 +30,3 @@ type UserInformationRequestBody struct {
 	StudentEvidence string             `json:"studentEvidence"`
 	Lifestyles      []domain.Lifestyle `json:"lifestyles" gorm:"default:null"`
 }
-
-type UserInformationUpdateBody struct {
-	Username        string            `json:"username" gorm:"unique"`
-	Password        string            `json:"password" validate:"omitempty,min=8"`
-	Firstname       string            `json:"firstname"`
-	Lastname        string            `json:"lastname"`
-	NationalID      string            `json:"nationalID"`
-	Gender          string            `json:"gender"`
-	BirthDate       time.Time         `json:"birthDate"`
-	StudentEvidence string            `json:"studentEvidence"`
-	Lifestyle1      *domain.Lifestyle `json:"lifestyle1" gorm:"default:null"`
-	Lifestyle2      *domain.Lifestyle `json:"lifestyle2" gorm:"default:null"`
-	Lifestyle3      *domain.Lifestyle `json:"lifestyle3" gorm:"default:null"`
-}
