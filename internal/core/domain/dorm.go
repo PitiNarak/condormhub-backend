0aeec325eeec24f4a74fb3d624eb071c6cea9a0e
Chantouch Narasetaphan <inecrft4747@gmail.com>
2025-02-15 16:40:29 +0700

    fix: add json "-" to ignore Owner field

--- internal/core/domain/dorm.go
+++ internal/core/domain/dorm.go
@@ -12,13 +12,13 @@ type Dorm struct {
 	UpdateAt    time.Time `json:"updateAt" gorm:"autoUpdateTime"`
 	Name        string    `json:"name" validate:"required"`
 	OwnerID     uuid.UUID `json:"ownerId" validate:"required"`
-	Owner       User
-	Size        float64 `json:"size" validate:"required,gt=0"`
-	Bedrooms    int     `json:"bedrooms" validate:"required,gte=0"`
-	Bathrooms   int     `json:"bathrooms" validate:"required,gte=0"`
-	Province    string  `json:"province" validate:"required"`
-	District    string  `json:"district" validate:"required"`
-	Price       float64 `json:"price" validate:"required,gt=0"`
-	Rating      float64 `json:"rating" gorm:"default:0" validate:"gte=0,lte=5"`
-	Description string  `json:"description" gorm:"type:text"`
+	Owner       User      `json:"-"`
+	Size        float64   `json:"size" validate:"required,gt=0"`
+	Bedrooms    int       `json:"bedrooms" validate:"required,gte=0"`
+	Bathrooms   int       `json:"bathrooms" validate:"required,gte=0"`
+	Province    string    `json:"province" validate:"required"`
+	District    string    `json:"district" validate:"required"`
+	Price       float64   `json:"price" validate:"required,gt=0"`
+	Rating      float64   `json:"rating" gorm:"default:0" validate:"gte=0,lte=5"`
+	Description string    `json:"description" gorm:"type:text"`
 }
