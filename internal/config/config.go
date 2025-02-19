4be0d206b1b5e205ea20ac8d6ee47cfbd1059799
Thanapon Johdee <sern.dev@gmail.com>
2025-02-20 01:58:41 +0700

    feat: add pkg stripe

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,17 +7,19 @@ import (
 	"github.com/PitiNarak/condormhub-backend/internal/databases"
 	"github.com/PitiNarak/condormhub-backend/internal/server"
 	"github.com/PitiNarak/condormhub-backend/internal/storage"
+	"github.com/PitiNarak/condormhub-backend/pkg/stripe"
 	"github.com/PitiNarak/condormhub-backend/pkg/utils"
 	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
 )
 
 type AppConfig struct {
-	SMTP     services.SMTPConfig `envPrefix:"SMTP_"`
-	JWT      utils.JWTConfig     `envPrefix:"JWT_"`
-	Server   server.Config       `envPrefix:"SERVER_"`
-	Database databases.Config    `envPrefix:"DB_"`
-	Storage  storage.Config      `envPrefix:"STORAGE_"`
+	SMTP         services.SMTPConfig `envPrefix:"SMTP_"`
+	JWT          utils.JWTConfig     `envPrefix:"JWT_"`
+	Server       server.Config       `envPrefix:"SERVER_"`
+	Database     databases.Config    `envPrefix:"DB_"`
+	Storage      storage.Config      `envPrefix:"STORAGE_"`
+	StripeConfig stripe.Config       `envPrefix:"STRIPE_"`
 }
 
 // Load configs from .env file
---
+++ pkg/stripe/stripe.go
@@ -0,0 +1,26 @@
+package stripe
+
+import (
+	"github.com/stripe/stripe-go/v81"
+	"github.com/stripe/stripe-go/v81/checkout/session"
+)
+
+type Config struct {
+	StripePublishableKey string `env:"PUBLISHABLE_KEY"`
+	StripeSecretKey      string `env:"SECRET_KEY"`
+	StripeSuccessURL     string `env:"SUCCESS_URL"`
+	StripeCancelURL      string `env:"CANCEL_URL"`
+}
+
+type Stripe struct {
+	config Config
+}
+
+func New(config Config) *Stripe {
+	return &Stripe{config: config}
+}
+
+func (s *Stripe) CreateSession(params *stripe.CheckoutSessionParams) (*stripe.CheckoutSession, error) {
+	stripe.Key = s.config.StripeSecretKey
+	return session.New(params)
+}
