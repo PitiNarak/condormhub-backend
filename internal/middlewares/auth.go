b8c1102907aa7258b77e261b493f4ba2907e9b00
Thanapon Johdee <sern.dev@gmail.com>
2025-02-23 05:55:07 +0700

    feat: simplify token verification in auth middleware by using VerifyAccessToken

--- internal/middlewares/auth.go
+++ internal/middlewares/auth.go
@@ -3,13 +3,11 @@ package middlewares
 import (
 	"errors"
 	"strings"
-	"time"
 
 	"github.com/PitiNarak/condormhub-backend/internal/core/ports"
 	"github.com/PitiNarak/condormhub-backend/pkg/errorHandler"
 	"github.com/PitiNarak/condormhub-backend/pkg/jwt"
 	"github.com/gofiber/fiber/v2"
-	"github.com/google/uuid"
 )
 
 type AuthMiddleware struct {
@@ -36,26 +34,17 @@ func (a *AuthMiddleware) Auth(ctx *fiber.Ctx) error {
 	}
 
 	token := authHeader[7:]
-	claims, err := a.jwtUtils.DecodeJWT(token)
+	userID, err := a.jwtUtils.VerifyAccessToken(ctx.Context(), token)
 	if err != nil {
 		return errorHandler.UnauthorizedError(err, "Invalid token")
 	}
 
-	if claims.GetExp() < time.Now().Unix() {
-		return errorHandler.UnauthorizedError(errors.New("token expired"), "Token is expired")
-	}
-
-	uuid, err := uuid.Parse(claims.GetUserID())
-	if err != nil {
-		return errorHandler.UnauthorizedError(err, "Invalid user ID")
-	}
-
-	user, err := a.userRepo.GetUserByID(uuid)
+	user, err := a.userRepo.GetUserByID(userID)
 	if err != nil {
 		return errorHandler.UnauthorizedError(err, "User not found")
 	}
 
-	ctx.Locals("userID", claims.GetUserID())
+	ctx.Locals("userID", userID)
 	ctx.Locals("user", user)
 
 	return ctx.Next()
