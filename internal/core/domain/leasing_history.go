0e05e120fbdc29383f340c828f804fd89e4610dd
Pongpak Manoret <pongpak282546@gmail.com>
2025-02-22 22:59:58 +0700

    Feat: add port for leasing history

--- internal/core/domain/leasing_history.go
+++ internal/core/domain/leasing_history.go
@@ -1,6 +1,10 @@
 package domain
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type LeasingHistory struct {
 	ID       uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
@@ -9,4 +13,6 @@ type LeasingHistory struct {
 	LesseeID uuid.UUID `json:"lessee_id" gorm:"type:uuid;not null"`
 	Lessee   User      `json:"lessee" gorm:"foreignKey:LesseeID;references:ID"`
 	Orders   []Order   `json:"orders" gorm:"foreignKey:LeasingHistoryID"`
+	Start    time.Time `json:"start"`
+	End      time.Time `json:"end"`
 }
