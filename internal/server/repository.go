3ec7725ef1c3e8d877aca5ba74cdcd4cbcb69b3e
Pongpak Manoret <pongpak282546@gmail.com>
2025-02-23 20:50:57 +0700

    feat: add leasing history handler service and repo to server

--- internal/server/repository.go
+++ internal/server/repository.go
@@ -6,15 +6,18 @@ import (
 )
 
 type repository struct {
-	user ports.UserRepository
-	dorm ports.DormRepository
+	user           ports.UserRepository
+	dorm           ports.DormRepository
+	leasingHistory ports.LeasingHistoryRepository
 }
 
 func (s *Server) initRepository() {
 	user := repositories.NewUserRepo(s.db)
 	dorm := repositories.NewDormRepository(s.db)
+	leasingHistory := repositories.NewLeasingHistoryRepository(s.db)
 	s.repository = &repository{
-		user: user,
-		dorm: dorm,
+		user:           user,
+		dorm:           dorm,
+		leasingHistory: leasingHistory,
 	}
 }
--- internal/server/service.go
+++ internal/server/service.go
@@ -6,19 +6,22 @@ import (
 )
 
 type service struct {
-	email ports.EmailServicePort
-	user  ports.UserService
-	dorm  ports.DormService
+	email          ports.EmailServicePort
+	user           ports.UserService
+	dorm           ports.DormService
+	leasingHistory ports.LeasingHistoryService
 }
 
 func (s *Server) initService() {
 	email := services.NewEmailService(s.smtpConfig, s.jwtUtils)
 	user := services.NewUserService(s.repository.user, email, s.jwtUtils)
 	dorm := services.NewDormService(s.repository.dorm)
+	leasingHistory := services.NewLeasingHistoryService(s.repository.leasingHistory)
 
 	s.service = &service{
-		email: email,
-		user:  user,
-		dorm:  dorm,
+		email:          email,
+		user:           user,
+		dorm:           dorm,
+		leasingHistory: leasingHistory,
 	}
 }
