e8ffe3b3dc83b8490cf1183e3969fcca8e43c898
Thanapon Johdee <sern.dev@gmail.com>
2025-02-26 04:30:50 +0700

    feat: update OrderHandler methods to use fiber.Context for improved request handling

--- internal/core/ports/order_port.go
+++ internal/core/ports/order_port.go
@@ -3,6 +3,7 @@ package ports
 import (
 	"github.com/PitiNarak/condormhub-backend/internal/core/domain"
 	"github.com/PitiNarak/condormhub-backend/pkg/errorHandler"
+	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 )
 
@@ -23,9 +24,9 @@ type OrderService interface {
 }
 
 type OrderHandler interface {
-	CreateOrder(order *domain.Order) *errorHandler.ErrorHandler
-	GetOrder(orderID uuid.UUID) (*domain.Order, *errorHandler.ErrorHandler)
-	GetUnpaidOrderByUserID(userID uuid.UUID, limit int, page int) ([]domain.Order, int, int, *errorHandler.ErrorHandler)
-	UpdateOrder(order *domain.Order) *errorHandler.ErrorHandler
-	DeleteOrder(orderID uuid.UUID) *errorHandler.ErrorHandler
+	CreateOrder(c *fiber.Ctx) *errorHandler.ErrorHandler
+	GetOrder(c *fiber.Ctx) (*domain.Order, *errorHandler.ErrorHandler)
+	GetUnpaidOrderByUserID(c *fiber.Ctx) ([]domain.Order, int, int, *errorHandler.ErrorHandler)
+	UpdateOrder(c *fiber.Ctx) *errorHandler.ErrorHandler
+	DeleteOrder(c *fiber.Ctx) *errorHandler.ErrorHandler
 }
