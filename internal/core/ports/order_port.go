edb103a573c565986805ea3f3dbc69bf691c8683
Thanapon Johdee <sern.dev@gmail.com>
2025-02-26 04:27:45 +0700

    feat: add OrderService and OrderHandler interfaces for order management

--- internal/core/ports/order_port.go
+++ internal/core/ports/order_port.go
@@ -9,7 +9,23 @@ import (
 type OrderRepository interface {
 	Create(order *domain.Order) *errorHandler.ErrorHandler
 	GetByID(orderID uuid.UUID) (*domain.Order, *errorHandler.ErrorHandler)
-	GetAll() ([]*domain.Order, *errorHandler.ErrorHandler)
+	GetUnpaidByUserID(userID uuid.UUID, limit int, page int) ([]domain.Order, int, int, *errorHandler.ErrorHandler)
 	Update(order *domain.Order) *errorHandler.ErrorHandler
 	Delete(orderID uuid.UUID) *errorHandler.ErrorHandler
+}
+
+type OrderService interface {
+	CreateOrder(leasingHistoryID uuid.UUID) *errorHandler.ErrorHandler
+	GetOrderByID(orderID uuid.UUID) (*domain.Order, *errorHandler.ErrorHandler)
+	GetUnpaidOrderByUserID(userID uuid.UUID, limit int, page int) ([]domain.Order, int, int, *errorHandler.ErrorHandler)
+	UpdateOrder(order *domain.Order) *errorHandler.ErrorHandler
+	DeleteOrder(orderID uuid.UUID) *errorHandler.ErrorHandler
+}
+
+type OrderHandler interface {
+	CreateOrder(order *domain.Order) *errorHandler.ErrorHandler
+	GetOrder(orderID uuid.UUID) (*domain.Order, *errorHandler.ErrorHandler)
+	GetUnpaidOrderByUserID(userID uuid.UUID, limit int, page int) ([]domain.Order, int, int, *errorHandler.ErrorHandler)
+	UpdateOrder(order *domain.Order) *errorHandler.ErrorHandler
+	DeleteOrder(orderID uuid.UUID) *errorHandler.ErrorHandler
 }
