9b2d395e009872a94d32e62821d9a8352e262b7b
Thanapon Johdee <sern.dev@gmail.com>
2025-03-10 17:30:59 +0700

    refactor: merged get info

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -310,3 +310,19 @@ func (h *UserHandler) Login(c *fiber.Ctx) error {
 
 	return c.Status(fiber.StatusOK).JSON(res)
 }
+
+// GetUserInfo godoc
+// @Summary Get user information
+// @Description Get user information
+// @Tags user
+// @Security Bearer
+// @Produce json
+// @Success 200 {object} dto.SuccessResponse[domain.User] "get user information successfully"
+// @Failure 401 {object} dto.ErrorResponse "your request is unauthorized"
+// @Failure 500 {object} dto.ErrorResponse "system cannot get user information"
+// @Router /user/me [get]
+func (h *UserHandler) GetUserInfo(c *fiber.Ctx) error {
+	user := c.Locals("user").(*domain.User)
+	res := dto.Success(user)
+	return c.Status(fiber.StatusOK).JSON(res)
+}
