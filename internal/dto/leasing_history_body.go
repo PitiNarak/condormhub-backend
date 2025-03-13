cd26e8a20610e82d09a4742c80eb967724d1cdda
Thanapon Johdee <sern.dev@gmail.com>
2025-03-13 15:25:18 +0700

    fix: remove gorm tag in dto

--- internal/dto/leasing_history_body.go
+++ internal/dto/leasing_history_body.go
@@ -7,12 +7,12 @@ import (
 )
 
 type LeasingHistory struct {
-	ID       uuid.UUID           `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	Dorm     DormResponseBody    `json:"dorm" gorm:"foreignKey:DormID;references:ID"`
-	LesseeID uuid.UUID           `json:"lessee_id" gorm:"type:uuid;not null"`
-	Lessee   UserResponse        `json:"lessee" gorm:"foreignKey:LesseeID;references:ID"`
-	Orders   []OrderResponseBody `json:"orders" gorm:"foreignKey:LeasingHistoryID"`
+	ID       uuid.UUID           `json:"id"`
+	Dorm     DormResponseBody    `json:"dorm"`
+	LesseeID uuid.UUID           `json:"lessee_id"`
+	Lessee   UserResponse        `json:"lessee"`
+	Orders   []OrderResponseBody `json:"orders"`
 	Start    time.Time           `json:"start"`
-	End      time.Time           `json:"end" gorm:"default:null"`
+	End      time.Time           `json:"end"`
 	Price    float64             `json:"price"`
 }
