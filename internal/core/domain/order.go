f448f5aaf02a08df21e2c1602c6f9ea5402e0b00
Thanapon Johdee <sern.dev@gmail.com>
2025-03-13 12:38:58 +0700

    fix: remove pointer as much as possible

--- internal/core/domain/order.go
+++ internal/core/domain/order.go
@@ -15,16 +15,16 @@ const (
 )
 
 type Order struct {
-	ID                uuid.UUID       `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	CreateAt          time.Time       `json:"createAt" gorm:"autoCreateTime"`
-	UpdateAt          time.Time       `json:"updateAt" gorm:"autoUpdateTime"`
-	Type              OrderType       `json:"type"`
-	Price             int64           `json:"price"`
-	Transactions      []*Transaction  `json:"transactions" gorm:"foreignKey:OrderID"`
-	PaidTransaction   *Transaction    `json:"paidTransaction" gorm:"foreignKey:OrderID;default:null"`
-	PaidTransactionID string          `json:"paidTransactionID" gorm:"default:null"`
-	LeasingHistory    *LeasingHistory `json:"leasingHistory" gorm:"foreignKey:LeasingHistoryID"`
-	LeasingHistoryID  uuid.UUID       `json:"leasingHistoryID"`
+	ID                uuid.UUID      `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	CreateAt          time.Time      `json:"createAt" gorm:"autoCreateTime"`
+	UpdateAt          time.Time      `json:"updateAt" gorm:"autoUpdateTime"`
+	Type              OrderType      `json:"type"`
+	Price             int64          `json:"price"`
+	Transactions      []*Transaction `json:"transactions" gorm:"foreignKey:OrderID"`
+	PaidTransaction   *Transaction   `json:"paidTransaction" gorm:"foreignKey:OrderID;default:null"`
+	PaidTransactionID string         `json:"paidTransactionID" gorm:"default:null"`
+	LeasingHistory    LeasingHistory `json:"leasingHistory" gorm:"foreignKey:LeasingHistoryID"`
+	LeasingHistoryID  uuid.UUID      `json:"leasingHistoryID"`
 }
 
 func (o *Order) ToDTO() dto.OrderResponseBody {
