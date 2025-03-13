7f552a3123b0723d3277b22d18d02d1b4f049cb4
Thanapon Johdee <sern.dev@gmail.com>
2025-03-13 15:37:59 +0700

    fix: remove unnecessary field

--- internal/dto/leasing_history_body.go
+++ internal/dto/leasing_history_body.go
@@ -7,12 +7,11 @@ import (
 )
 
 type LeasingHistory struct {
-	ID       uuid.UUID           `json:"id"`
-	Dorm     DormResponseBody    `json:"dorm"`
-	LesseeID uuid.UUID           `json:"lessee_id"`
-	Lessee   UserResponse        `json:"lessee"`
-	Orders   []OrderResponseBody `json:"orders"`
-	Start    time.Time           `json:"start"`
-	End      time.Time           `json:"end"`
-	Price    float64             `json:"price"`
+	ID     uuid.UUID           `json:"id"`
+	Dorm   DormResponseBody    `json:"dorm"`
+	Lessee UserResponse        `json:"lessee"`
+	Orders []OrderResponseBody `json:"orders"`
+	Start  time.Time           `json:"start"`
+	End    time.Time           `json:"end"`
+	Price  float64             `json:"price"`
 }
