78ebcb7d81a91815c3e38dac8bf762a762946810
Pongpak Manoret <pongpak282546@gmail.com>
2025-03-20 19:09:09 +0700

    refactor: delete apperror.IsAppError

--- internal/repository/leasing_request_repository.go
+++ internal/repository/leasing_request_repository.go
@@ -21,9 +21,6 @@ func NewLeasingRequestRepository(db *database.Database) ports.LeasingRequestRepo
 
 func (d *LeasingRequestRepository) Create(LeasingRequest *domain.LeasingRequest) error {
 	if err := d.db.Create(LeasingRequest).Error; err != nil {
-		if apperror.IsAppError(err) {
-			return err
-		}
 		return apperror.InternalServerError(err, "failed to save leasing request to database")
 	}
 	return nil
@@ -32,9 +29,6 @@ func (d *LeasingRequestRepository) Create(LeasingRequest *domain.LeasingRequest)
 func (d *LeasingRequestRepository) GetByID(id uuid.UUID) (*domain.LeasingRequest, error) {
 	leasingRequest := new(domain.LeasingRequest)
 	if err := d.db.Preload("Dorm").Preload("Lessee").First(leasingRequest, id).Error; err != nil {
-		if apperror.IsAppError(err) {
-			return nil, err
-		}
 		return nil, apperror.NotFoundError(err, "leasing request not found")
 	}
 	return leasingRequest, nil
@@ -43,25 +37,16 @@ func (d *LeasingRequestRepository) GetByID(id uuid.UUID) (*domain.LeasingRequest
 func (d *LeasingRequestRepository) Update(LeasingRequest *domain.LeasingRequest) error {
 	existingRequest, err := d.GetByID(LeasingRequest.ID)
 	if err != nil {
-		if apperror.IsAppError(err) {
-			return err
-		}
 		return apperror.NotFoundError(err, "leasing request not found")
 	}
 	err = d.db.Model(existingRequest).Updates(LeasingRequest).Error
 	if err != nil {
-		if apperror.IsAppError(err) {
-			return err
-		}
 		return apperror.InternalServerError(err, "failed to update leasing request")
 	}
 	return nil
 }
 func (d *LeasingRequestRepository) Delete(id uuid.UUID) error {
 	if err := d.db.Delete(&domain.LeasingRequest{}, id).Error; err != nil {
-		if apperror.IsAppError(err) {
-			return err
-		}
 		return apperror.InternalServerError(err, "failed to delete leasing request")
 	}
 	return nil
@@ -77,9 +62,6 @@ func (d *LeasingRequestRepository) GetByUserID(id uuid.UUID, limit, page int, ro
 	totalPage, totalRows, err := d.db.Paginate(&leasingRequest, query, limit, page, "start DESC")
 
 	if err != nil {
-		if apperror.IsAppError(err) {
-			return nil, 0, 0, err
-		}
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, 0, 0, apperror.NotFoundError(err, "leasing request not found")
 		}
