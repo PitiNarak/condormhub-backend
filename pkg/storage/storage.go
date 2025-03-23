90319fb25c50b8ee1c8a1516ca16e637114a1277
Chantouch Narasetaphan <inecrft4747@gmail.com>
2025-03-23 15:04:21 +0700

    feat: implement function to extract file key from url

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net/url"
 	"time"
 
+	"github.com/PitiNarak/condormhub-backend/pkg/apperror"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
@@ -142,4 +144,13 @@ func (s *Storage) GetSignedUrl(ctx context.Context, key string, expires time.Dur
 
 func (s *Storage) GetPublicUrl(key string) string {
 	return fmt.Sprintf(s.Config.URL_PREFIX, key)
+}
+
+func (s *Storage) GetFileKeyFromPublicUrl(imageURL string) (string, error) {
+	parsedURL, err := url.Parse(imageURL)
+	if err != nil {
+		return "", apperror.InternalServerError(err, "Failed to parse URL")
+	}
+
+	return parsedURL.Path, nil
 }
