06f3c3d768f230e0cf3058334e6709254aa8a4dd
Chantouch Narasetaphan <inecrft4747@gmail.com>
2025-04-08 10:15:43 +0700

    docs: gen swagger doc for create support

--- internal/handler/support_handler.go
+++ internal/handler/support_handler.go
@@ -18,6 +18,19 @@ func NewSupportHandler(service ports.SupportService) ports.SupportHandler {
 	return &SupportHandler{service: service}
 }
 
+// Create godoc
+// @Summary Submit a support request
+// @Description Let a user send a message to the admin
+// @Tags support
+// @Security Bearer
+// @Accept json
+// @Produce json
+// @Param request body dto.SupportRequestBody true "Support Request"
+// @Success 201 {object} dto.SuccessResponse[dto.SupportResponseBody] "Support request submitted successfully"
+// @Failure 400 {object} dto.ErrorResponse "Your request is invalid"
+// @Failure 401 {object} dto.ErrorResponse "your request is unauthorized"
+// @Failure 500 {object} dto.ErrorResponse "Could not submit support request"
+// @Router /support [post]
 func (h *SupportHandler) Create(c *fiber.Ctx) error {
 	reqBody := new(dto.SupportRequestBody)
 	if err := c.BodyParser(reqBody); err != nil {
