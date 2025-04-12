406db982ddfa64d4fb12cc5fe8ea66334f158636
Chantouch Narasetaphan <inecrft4747@gmail.com>
2025-04-12 14:15:10 +0700

    feat: Add endpoint to route

--- internal/server/route.go
+++ internal/server/route.go
@@ -142,4 +142,5 @@ func (s *Server) initSupportRoutes() {
 	supportRoutes := s.app.Group("/support")
 	supportRoutes.Post("/", s.authMiddleware.Auth, s.handler.support.Create)
 	supportRoutes.Get("/", s.authMiddleware.Auth, s.handler.support.GetAll)
+	supportRoutes.Patch("/:id", s.authMiddleware.Auth, s.handler.support.UpdateStatus)
 }
