dcd75c0e0d2ebb61610c35beef5f9880087c89a4
Chantouch Narasetaphan <inecrft4747@gmail.com>
2025-04-12 13:51:05 +0700

    feat: Implement GetByID inside service and repo layer

--- internal/core/ports/support_ports.go
+++ internal/core/ports/support_ports.go
@@ -9,12 +9,14 @@ import (
 type SupportRepository interface {
 	Create(support *domain.SupportRequest) error
 	GetAll(limit int, page int, userID uuid.UUID, isAdmin bool) ([]domain.SupportRequest, int, int, error)
+	GetByID(id uuid.UUID) (*domain.SupportRequest, error)
 	UpdateStatus(id uuid.UUID, status domain.SupportStatus) error
 }
 
 type SupportService interface {
 	Create(support *domain.SupportRequest) error
 	GetAll(limit int, page int, userID uuid.UUID, isAdmin bool) ([]domain.SupportRequest, int, int, error)
+	GetByID(id uuid.UUID) (*domain.SupportRequest, error)
 	UpdateStatus(id uuid.UUID, status domain.SupportStatus) error
 }
 
